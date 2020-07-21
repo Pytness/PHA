765d1e2610b2da479285f1a1b09fc86861c56f45
Pytness <pytness@gmail.com>
2020-07-21 16:25:42 +0200

    Update

--- src/sha2/sha2.hpp
+++ src/sha2/sha2.hpp
@@ -1,33 +1,21 @@
 #ifndef SHA2_H
 #define SHA2_H
 
-#define SHA256_DIGEST_SIZE ( 256 / 8)
-#define SHA256_BLOCK_SIZE  ( 512 / 8)
+#include <stdint.h>
 
-#define SHFR(x, n)    (x >> n)
-#define ROTR(x, n)   ((x >> n) | (x << ((sizeof(x) << 3) - n)))
-#define CH(x, y, z)  ((x & y) ^ (~x & z))
-#define MAJ(x, y, z) ((x & y) ^ (x & z) ^ (y & z))
-
-#define SHA256_F1(x) (ROTR(x,  2) ^ ROTR(x, 13) ^ ROTR(x, 22))
-#define SHA256_F2(x) (ROTR(x,  6) ^ ROTR(x, 11) ^ ROTR(x, 25))
-#define SHA256_F3(x) (ROTR(x,  7) ^ ROTR(x, 18) ^ SHFR(x,  3))
-#define SHA256_F4(x) (ROTR(x, 17) ^ ROTR(x, 19) ^ SHFR(x, 10))
+#define SHA256_DIGEST_SIZE 32
+#define SHA256_BLOCK_SIZE  64
 
 typedef struct {
-    unsigned int tot_len;
-    unsigned int len;
-    unsigned char block[2 * SHA256_BLOCK_SIZE];
-    uint32_t h[8];
+	uint64_t tot_len;
+	uint64_t len;
+	uint8_t block[2 * SHA256_BLOCK_SIZE];
+	uint32_t hash[8];
 } sha256_ctx;
 
-extern uint32_t sha256_k[64];
-
 void sha256_init(sha256_ctx * ctx);
-void sha256_update(sha256_ctx *ctx, const unsigned char *message,
-                   unsigned int len);
-void sha256_final(sha256_ctx *ctx, unsigned char *digest);
-void sha256(const unsigned char *message, unsigned int len,
-            unsigned char *digest);
+void sha256_update(sha256_ctx * ctx, const uint8_t * message, uint64_t len);
+void sha256_final(sha256_ctx * ctx, uint8_t * digest);
+void sha256(const uint8_t * message, uint64_t len, uint8_t * digest);
 
-#endif 
+#endif
